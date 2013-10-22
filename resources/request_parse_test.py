67406d6d50526e46464506dd4f1563fcc5c7d24f
john <thejohnskauge@gmail.com>
2013-10-22 16:53:40 +0100

    Corrected fields usage

--- resources/request_parse_test.py
+++ resources/request_parse_test.py
@@ -1,14 +1,14 @@
 from datetime import datetime
-from flask.ext.restful import Resource, marshal_with, reqparse
-from mongoengine import fields, ValidationError
+from flask.ext.restful import Resource, marshal_with, reqparse, fields
+
 from models.user import User
 
 __author__ = 'Modulus'
 
 fields = {
-    "firstName": fields.StringField,
-    "lastName": fields.StringField,
-    "userName": fields.StringField
+    "firstName": fields.String,
+    "lastName": fields.String,
+    "userName": fields.String
 
 }
 
@@ -22,16 +22,15 @@ class RequestParseTest(Resource):
         self.parser.add_argument("user_name", type=str, help="User name is needed")
         self.args = self.parser.parse_args()
 
-    @marshal_with(User.format())
+    @marshal_with(fields)
     def post(self):
-        try:
-            first_name = self.args["first_name"]
-            last_name = self.args["last_name"]
-            user_name = self.args["user_name"]
 
-            user = User(firstName=first_name, lastName=last_name, userName=user_name, birthDate=datetime.utcnow())
-            user.save()
-            return user
-        except ValidationError:
-            return {"error": "Missing required fields"}
+        first_name = self.args["first_name"]
+        last_name = self.args["last_name"]
+        user_name = self.args["user_name"]
+
+        user = User(firstName=first_name, lastName=last_name, userName=user_name, birthDate=datetime.utcnow())
+        user.save()
+        return user
+
 
